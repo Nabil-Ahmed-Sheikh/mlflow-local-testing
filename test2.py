a4499ac3d804597f0946829325b6e2d172aed1ad
Nabil Ahmed <nabil.ahmednsu@gmail.com>
2022-10-23 13:43:44 +0600

    updated gitignore

--- test2.py
+++ test2.py
@@ -13,24 +13,25 @@ import mlflow.sklearn
 
 import logging
 
-code_version="13"
+code_version = "13"
 
-os.environ["SIDETREK_PROJECT_ID"]="a7c7184b-26c5-4e38-bb0a-cb3d89d0da74"
-os.environ["SIDETREK_USER_ID"]="1989f0db-a509-433f-8223-2a463ead81c0"
-os.environ["SIDETREK_ORGANIZATION_ID"]="e503b337-c3b5-455d-a744-9f1098c71ec2"
+os.environ["SIDETREK_PROJECT_ID"] = "a7c7184b-26c5-4e38-bb0a-cb3d89d0da74"
+os.environ["SIDETREK_USER_ID"] = "1989f0db-a509-433f-8223-2a463ead81c0"
+os.environ["SIDETREK_ORGANIZATION_ID"] = "e503b337-c3b5-455d-a744-9f1098c71ec2"
 
 exp_name = "Test Experiment " + code_version
-reg_model_name="ElasticnetWineModel"+ code_version
+reg_model_name = "ElasticnetWineModel" + code_version
 
-mlflow.set_tracking_uri("http://127.0.0.1:4000/p/")
+mlflow.set_tracking_uri("https://mlflow.sidetrek.com/p")
 experiment_id = mlflow.create_experiment(
-        exp_name,
-        tags={"version": "v1", "priority": "P1"},
-    )
+    exp_name,
+    tags={"version": "v1", "priority": "P1"},
+)
 
 print(mlflow.get_experiment_by_name(exp_name))
 
-os.environ["MLFLOW_EXPERIMENT_NAME"] = mlflow.get_experiment_by_name(exp_name).name
+os.environ["MLFLOW_EXPERIMENT_NAME"] = mlflow.get_experiment_by_name(
+    exp_name).name
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -71,9 +72,9 @@ if __name__ == "__main__":
     l1_ratio = 1
 
     # mlflow.set_tracking_uri("http://a39be1be3c4a64723a75119ec661f1c7-913060790.us-west-2.elb.amazonaws.com:5000")
-    
+
     with mlflow.start_run():
-        
+
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
@@ -101,6 +102,7 @@ if __name__ == "__main__":
             # There are other ways to use the Model Registry, which depends on the use case,
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(lr, "model", registered_model_name=reg_model_name)
+            mlflow.sklearn.log_model(
+                lr, "model", registered_model_name=reg_model_name)
         else:
             mlflow.sklearn.log_model(lr, "model")
